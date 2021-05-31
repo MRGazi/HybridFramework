5aa8a82825e6bbf181134e96c3bb2b1bf41c0720
MRGazi <ar.mrg.ny@gmail.com>
2021-05-31 15:24:25 -0400

    SeleniumMavenProject01

---
+++ src/main/java/Calculation/TestMain.java
@@ -0,0 +1,10 @@
+package Calculation;
+
+public class TestMain {
+    static  int a = 20;
+    static  int b = 50;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Total = " + (a + b));
+    }
+}
